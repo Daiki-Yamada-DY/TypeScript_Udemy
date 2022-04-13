dc05f641558cdeb2db963201fd09b042a777c9d1
Daiki-Yamada-DY <daiki.yamada.0830@hotmail.co.jp>
2022-04-13 23:16:24 +0900

    セクション7「96.Classに対してジェネリクスを使用する方法」までをコミット

--- dist/generics.js
+++ dist/generics.js
@@ -1,6 +1,27 @@
 "use strict";
 // 型パラメータに制約を設ける場合
 function copy(value) {
+    value;
     return value;
 }
 console.log(copy({ name: 'Quill' }));
+class LightDatabase {
+    constructor() {
+        this.data = [];
+    }
+    add(item) {
+        this.data.push(item);
+    }
+    remove(item) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringLightDatabase = new LightDatabase();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana');
+console.log(stringLightDatabase.get());
--- src/generics.ts
+++ src/generics.ts
@@ -1,6 +1,25 @@
 // 型パラメータに制約を設ける場合
 function copy<T extends { name: string }>(value: T): T {
+    value
     return value;
 }
 console.log(copy({ name: 'Quill' }));
 
+class LightDatabase<T extends string | number | boolean> {
+    private data: T[] = [];
+    add(item: T) {
+        this.data.push(item);
+    }
+    remove(item: T) {
+        this.data.splice(this.data.indexOf(item), 1);
+    }
+    get() {
+        return this.data;
+    }
+}
+const stringLightDatabase = new LightDatabase<string>();
+stringLightDatabase.add('Apple');
+stringLightDatabase.add('Banana');
+stringLightDatabase.add('Grape');
+stringLightDatabase.remove('Banana')
+console.log(stringLightDatabase.get());
