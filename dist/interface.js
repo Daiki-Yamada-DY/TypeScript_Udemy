8c40596f0bedb3822b5cbac9209840dd5473ce00
Daiki-Yamada-DY <102786833+Daiki-Yamada-DY@users.noreply.github.com>
2022-04-13 09:35:20 +0900

    セクション5「65.これがTypeScriptの構造的部分型だ!」までをコミット

--- dist/interface.js
+++ dist/interface.js
@@ -7,6 +7,16 @@ class Developer {
     }
     ;
     greeting(message) {
-        console.log('Hello!');
+        console.log(message);
     }
 }
+const tmpDeveloper = {
+    name: 'Quill',
+    age: 38,
+    experience: 3,
+    greeting(message) {
+        console.log(message);
+    }
+};
+// userは少なくともHumanインターフェースに含まれるプロパティは使うことができる
+const user = new Developer('Quill', 38, 3);
--- src/interface.ts
+++ src/interface.ts
@@ -6,6 +6,16 @@ interface Human  {
 class Developer implements Human {
     constructor(public name: string, public age: number, public experience: number){};
     greeting(message: string) {
-        console.log('Hello!');
+        console.log(message);
     }
 }
+const tmpDeveloper = {
+    name: 'Quill',
+    age: 38,
+    experience: 3,
+    greeting(message: string) {
+        console.log(message);
+    }
+}
+// userは少なくともHumanインターフェースに含まれるプロパティは使うことができる
+const user: Human = new Developer('Quill', 38, 3);
